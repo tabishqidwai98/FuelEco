f6fbc92ae9b10eb7f9d08ee3352dcd17760f3bfb
killer2875 <67414589+killer2875@users.noreply.github.com>
2020-06-29 21:41:00 +0530

    Add files via upload

--- fueleco.py
+++ fueleco.py
@@ -23,36 +23,44 @@ def load_data1(rows = None):
 
 data_load_state = st.text('loading fuel data...')
 data = load_data(10000)
-data_load_state.text("loaded the Dataset")
 data2 = load_data1(10000)
-data_load_state.text("loaded the Dataset2")
+data_load_state.text("loaded the Datasets")
 
 st.subheader("View Raw Data")
 st.write(data)
+#cols = ["class","displ","trans","cyl","trans.dscr","cty","hwy"]
+#st_ms = st.multiselect("Columns", data2.columns.tolist(), default=cols)
+#st.write(data2)
 st.write(data2)
 
 st.markdown('<h1 style="color:red">Histogram distribution in Fuel</h1>',unsafe_allow_html=True)
-column = st.selectbox("select a column from the dataset", ['barrels08'])
+st.text("Dataset : data 1")
+column = st.selectbox("select a column from the dataset", ['barrels08','city08','co2TailpipeGpm','comb08','displ','engId','fuelCost08'])
 bins = st.slider("select number of bins",5,100,20)
 
 histogram = data[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
 st.pyplot()
 
+st.text("Dataset : data 2")
 column = st.selectbox("select a column from the dataset", ['year'])
 bins = st.slider("select number of bins",10,110,40)
 histogram = data2[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
 st.pyplot()
 
 st.subheader("Column Comparison in Dataset")
-xcol = st.sidebar.selectbox("X axis :select a column from the dataset", data.columns)
+st.text("Dataset : data 1")
+xcol = st.sidebar.header(Comparision Graph).selectbox("X axis :select a column from the dataset", data.columns)
 ycol = st.sidebar.selectbox("Y axis :select a column from the dataset", data.columns)
 fig = px.scatter(data,x=xcol, y=ycol,color='year')
 st.plotly_chart(fig,use_container_width=True)
 
-st.subheader("Correlation in columns with Seaborn")
-xcol = st.selectbox("X axis", data.columns)
-ycol = st.selectbox("Y axis", data.columns)
-sns.regplot(x=xcol, y=ycol,data=data)
+st.text("Dataset : data 2")
+xcol = st.sidebar.selectbox("X axis :select a column from the dataset", data2.columns)
+ycol = st.sidebar.selectbox("Y axis :select a column from the dataset", data2.columns)
+fig = px.scatter(data2,x=xcol, y=ycol,color='year')
+st.plotly_chart(fig,use_container_width=True)
+
+data.model.value_counts().head().plot(kind='pie')
 st.pyplot()
 
 data2.manufacturer.value_counts().head().plot(kind='pie')
