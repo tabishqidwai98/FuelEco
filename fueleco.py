14a211de706a7c7f3189b73b6ae22aaf9b0f9396
tabishqidwai98 <60247314+tabishqidwai98@users.noreply.github.com>
2020-07-26 02:19:14 +0530

    commits

--- fueleco.py
+++ fueleco.py
@@ -6,56 +6,137 @@ import plotly.figure_factory as ff
 import plotly.express as px
 import seaborn as sns
 import pickle
+from helper import cleanChangeVal
 
-st.title("Fuel Economy Dataset")
+st.markdown('<h1 style="color:red">Fuel Economy prediction</h1>',unsafe_allow_html=True)
 
 st.text("lets do some prediction")
 
-DATA_PATH = "vehicles.csv"
-DATA_PATH1 = "mpg.csv"
+vehiclesUS = "vehicles.csv"
+mpgUS = "mpg.csv"
+crudeoil = "datasets/crudeoil.csv"
+CrudeoilvsDiesel = "datasets/CrudeoilvsDiesel.csv"
+CrudeoilvsGasoline = "datasets/CrudeoilvsGasoline.csv"
+Diesel = "datasets/Diesel(new).csv"
+petrol = "datasets/petrol(new).csv"
 
-page = st.sidebar.selectbox("select a page",['Data Anaytics','AI Application','Diesel Prediction']) 
+page = st.sidebar.selectbox("select a page",['Data Anaytics','Mileage Prediction','Diesel Prediction','Petrol Prediction']) 
 if page =='Data Anaytics':    
 
-    @st.cache
-    def load_data(rows = None):
-        data = pd.read_csv(DATA_PATH, nrows = rows)
+    @st.cache()
+    def load_data_vehiclesUs(rows = None):
+        data = pd.read_csv(vehiclesUS, nrows = rows)
         data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
         return data
 
-    @st.cache
-    def load_data1(rows = None):
-        data = pd.read_csv(DATA_PATH1, nrows = rows)
+    def load_dataMpgUS_mpgUS(rows = None):
+        data = pd.read_csv(mpgUS, nrows = rows)
+        data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
+        return data
+    
+    def load_data2_crudeoil(rows = None):
+        data = pd.read_csv(crudeoil, nrows = rows)
         data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
         return data
 
+    def load_data3_CrudeoilvsDiesel(rows = None):
+        data = pd.read_csv(CrudeoilvsDiesel, nrows = rows, parse_dates=['Month'],dayfirst=True)
+        return data
+
+    def load_data4_CrudeoilvsGasoline(rows = None):
+        data = pd.read_csv(CrudeoilvsGasoline, nrows = rows)
+        data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
+        return data
+
+    def load_data5_Diesel(rows = None):
+        data = pd.read_csv(Diesel, nrows = rows)
+        data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
+        return data
+
+    def load_data6_petrol(rows = None):
+        data = pd.read_csv(petrol, nrows = rows)
+        data.rename(lambda col : str(col).lower(), axis ='columns', inplace = True)
+        return data
 
     data_load_state = st.text('loading fuel data...')
-    data = load_data(10000)
-    data2 = load_data1(10000)
+    datavehiclesUS = load_data_vehiclesUs(10000)
+    dataMpgUS = load_dataMpgUS_mpgUS(10000)
+    datacrudeoil = load_data2_crudeoil()
+    dataCrudeoilvsDiesel = load_data3_CrudeoilvsDiesel()
+    dataCrudeoilvsGasoline = load_data4_CrudeoilvsGasoline()
+    dataDiesel = load_data5_Diesel()
+    datapetrol = load_data6_petrol()
     data_load_state.text("loaded the Datasets")
 
     st.subheader("View Raw Data")
-    if st.checkbox("data 1"):
+    if st.checkbox("vehiclesUs"):
         st.write(data)
     #cols = ["class","displ","trans","cyl","trans.dscr","cty","hwy"]
-    #st_ms = st.multiselect("Columns", data2.columns.tolist(), default=cols)
-    #st.write(data2)
-    if st.checkbox("data 2"):
-        st.write(data2)
+    #st_ms = st.multiselect("Columns", dataMpgUS.columns.tolist(), default=cols)
+    #st.write(dataMpgUS)
+    if st.checkbox("mpgUS"):
+        st.write(dataMpgUS)
 
-    st.markdown('<h1 style="color:red">Histogram distribution in Fuel</h1>',unsafe_allow_html=True)
-    st.text("Dataset : data 1")
+    if st.checkbox("crudeoil"):
+        st.write(datacrudeoil)
+    
+    if st.checkbox("CrudeoilvsDiesel"):
+        st.write(dataCrudeoilvsDiesel)
+        st.write(dataCrudeoilvsDiesel.info)
+
+    if st.checkbox("CrudeoilvsGasoline"):
+        st.write(dataCrudeoilvsGasoline)
+
+    if st.checkbox("Diesel"):
+        st.write(dataDiesel)
+    
+    if st.checkbox("petrol"):
+        st.write(datapetrol)
+
+    st.subheader("Histogram distribution in Fuel")
+    st.text("Dataset : vehiclesUs")
     column = st.selectbox("select a column from the dataset", ['barrels08','city08','co2TailpipeGpm','comb08','displ','engId','fuelCost08'])
     bins = st.slider("select number of bins",5,100,20)
-
-    histogram = data[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    histogram = datavehiclesUS[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
     st.pyplot()
 
-    st.text("Dataset : data 2")
+    st.text("Dataset : MpgUS")
     column = st.selectbox("select a column from the dataset", ['year'])
     bins = st.slider("select number of bins",10,110,40)
-    histogram = data2[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    histogram = dataMpgUS[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    st.pyplot()
+
+    st.text("Dataset : crudeoil")
+    column = st.selectbox("select a column from the dataset", datacrudeoil.columns)
+    bins = st.slider("select number of bins",10,120,10)
+    histogram = datacrudeoil[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    st.pyplot()
+    
+    sns.regplot(x='month',y='price',data=datacrudeoil)
+    st.pyplot()
+
+    st.text("Dataset : CrudeoilvsDiesel")
+    column = st.selectbox("select a column from the dataset", dataCrudeoilvsDiesel.columns)
+    bins = st.slider("select number of bins",20,110,50)
+    histogram = dataCrudeoilvsDiesel[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    st.pyplot()
+
+    st.text("Dataset : CrudeoilvsGasoline")
+    column = st.selectbox("select a column from the dataset", dataCrudeoilvsGasoline.columns)
+    bins = st.slider("select number of bins",15,110,15)
+    histogram = dataCrudeoilvsGasoline[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    st.pyplot()
+
+    st.text("Dataset : Diesel")
+    column = st.selectbox("select a column from the dataset", dataDiesel.columns)
+    bins = st.slider("select number of bins",10,130,20)
+    histogram = dataDiesel[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
+    st.pyplot()
+
+    st.text("Dataset : petrol")
+    column = st.selectbox("select a column from the dataset", datapetrol.columns)
+    bins = st.slider("select number of bins",5,100,30)
+    histogram = datapetrol[column].plot.hist(bins=bins, title=f'{column} histogram analysis')
     st.pyplot()
     
     #st.subheader("Column Comparison in Dataset with bar graph")
@@ -65,16 +146,16 @@ if page =='Data Anaytics':
     #st.pyplot()
     # Data is not loading in the above graph even after re-running the streamlit
 
-    st.subheader("Column Compariso through scatter plot")
-    st.text("Dataset : data 1")
-    xcolumn = st.selectbox("select a column from the dataset for comparison", data.columns)
-    ycolumn1 = st.selectbox("select a first column for comparision with first column selected", data.columns)
-    ycolumn2 = st.selectbox("select a second column for comparosion with first column selected", data.columns)
-    ycolumn3 = st.selectbox("select a third column for comparosion with first column selected", data.columns)
+    st.subheader("Column Comparison through scatter plot")
+    st.text("Dataset : vehiclesUs")
+    xcolumn = st.selectbox("select a column from the dataset for comparison", datavehiclesUs.columns)
+    ycolumn1 = st.selectbox("select a first column for comparision with first column selected", datavehiclesUs.columns)
+    ycolumn2 = st.selectbox("select a second column for comparosion with first column selected", datavehiclesUs.columns)
+    ycolumn3 = st.selectbox("select a third column for comparosion with first column selected", datavehiclesUs.columns)
 
-    plt.scatter(xcolumn, ycolumn1, data = data )
-    plt.scatter(xcolumn, ycolumn2, data = data )
-    plt.scatter(xcolumn, ycolumn3, data = data )
+    plt.scatter(xcolumn, ycolumn1, data = datavehiclesUs )
+    plt.scatter(xcolumn, ycolumn2, data = datavehiclesUs )
+    plt.scatter(xcolumn, ycolumn3, data = datavehiclesUs )
     plt.xlabel(xcolumn)
     plt.ylabel('Consumption')
     plt.title('Comparison of columns of Dataset 1')
@@ -82,15 +163,15 @@ if page =='Data Anaytics':
     plt.legend()
     st.pyplot()
 
-    st.text("Dataset : data 2")
-    xcolumn = st.selectbox("select a column from the dataset for comparison", data2.columns)
-    ycolumn1 = st.selectbox("select a first column for comparision with first column selected", data2.columns)
-    ycolumn2 = st.selectbox("select a second column for comparosion with first column selected", data2.columns)
-    ycolumn3 = st.selectbox("select a third column for comparosion with first column selected", data2.columns)
+    st.text("Dataset : MpgUS")
+    xcolumn = st.selectbox("select a column from the dataset for comparison", dataMpgUS.columns)
+    ycolumn1 = st.selectbox("select a first column for comparision with first column selected", dataMpgUS.columns)
+    ycolumn2 = st.selectbox("select a second column for comparosion with first column selected", dataMpgUS.columns)
+    ycolumn3 = st.selectbox("select a third column for comparosion with first column selected", dataMpgUS.columns)
 
-    plt.scatter(xcolumn, ycolumn1, data = data2 )
-    plt.scatter(xcolumn, ycolumn2, data = data2 )
-    plt.scatter(xcolumn, ycolumn3, data = data2 )
+    plt.scatter(xcolumn, ycolumn1, data = dataMpgUS )
+    plt.scatter(xcolumn, ycolumn2, data = dataMpgUS )
+    plt.scatter(xcolumn, ycolumn3, data = dataMpgUS )
     plt.xlabel(xcolumn)
     plt.ylabel('Consumption')
     plt.title('Comparison of columns of Dataset 2')
@@ -99,46 +180,46 @@ if page =='Data Anaytics':
     st.pyplot()
 
     st.subheader("Column Comparison in Dataset")
-    st.text("Dataset : data 1")
+    st.text("Dataset : vehiclesUs")
     st.sidebar.header("Comparision Graph")
     xcol = st.sidebar.selectbox("X axis :select a column from the dataset", data.columns)
     ycol = st.sidebar.selectbox("Y axis :select a column from the dataset", data.columns)
     fig = px.scatter(data,x=xcol, y=ycol,color='year')
     st.plotly_chart(fig,use_container_width=True)
 
-    st.text("Dataset : data 2")
-    xcol = st.sidebar.selectbox("X axis :select a column from the dataset", data2.columns)
-    ycol = st.sidebar.selectbox("Y axis :select a column from the dataset", data2.columns)
-    fig = px.scatter(data2,x=xcol, y=ycol,color='year')
+    st.text("Dataset : MpgUS")
+    xcol = st.sidebar.selectbox("X axis :select a column from the dataset", dataMpgUS.columns)
+    ycol = st.sidebar.selectbox("Y axis :select a column from the dataset", dataMpgUS.columns)
+    fig = px.scatter(dataMpgUS,x=xcol, y=ycol,color='year')
     st.plotly_chart(fig,use_container_width=True)
 
     st.subheader("Pie chart comparision of vehicle")
-    st.text("Dataset : data 1")
+    st.text("Dataset : vehiclesUs")
     data.model.value_counts().head().plot(kind='pie')
     st.pyplot()
 
-    st.text("Dataset : data 2")
-    data2.manufacturer.value_counts().head().plot(kind='pie')
+    st.text("Dataset : MpgUS")
+    dataMpgUS.manufacturer.value_counts().head().plot(kind='pie')
     st.pyplot()
 
     #sns.set()
     #sns.heatmap(data.corr(), annot=True, fmt='.2f')
     #st.pyplot()
 
-    #sns.heatmap(data2.corr(), annot=True, fmt='.2f')
+    #sns.heatmap(dataMpgUS.corr(), annot=True, fmt='.2f')
     #st.pyplot()
 
     st.subheader("Column Comparison in Dataset through jointplot")
-    st.text('Dataset : Data 1')
-    xcolm = st.selectbox("X axis : select a column from the dataset", data.columns)
-    ycolm = st.selectbox("Y axis : select a column from the dataset", data.columns)
-    sns.jointplot(x=xcolm, y=ycolm, data=data)
+    st.text('Dataset : vehiclesUs')
+    xcolm = st.selectbox("X axis : select a column from the dataset", datavehiclesUs.columns)
+    ycolm = st.selectbox("Y axis : select a column from the dataset", datavehiclesUs.columns)
+    sns.jointplot(x=xcolm, y=ycolm, data=datavehiclesUs)
     st.pyplot()
 
-    st.text('Dataset : Data 2')
-    xcolm = st.selectbox("X axis : select a column from the dataset", data2.columns)
-    ycolm = st.selectbox("Y axis : select a column from the dataset", data2.columns)
-    sns.jointplot(x=xcolm, y=ycolm, data=data2)
+    st.text('Dataset : MpgUS')
+    xcolm = st.selectbox("X axis : select a column from the dataset", dataMpgUS.columns)
+    ycolm = st.selectbox("Y axis : select a column from the dataset", dataMpgUS.columns)
+    sns.jointplot(x=xcolm, y=ycolm, data=dataMpgUS)
     st.pyplot()
 
     #xcolm = st.selectbox("X axis : select a column from the dataset ", data.columns)
@@ -146,7 +227,7 @@ if page =='Data Anaytics':
     #sns.catplot(x=xcolm, y=ycolm, kind='violin', data=data)
     #st.pyplot()
 
-elif page == 'AI Application':
+elif page == 'Mileage Prediction':
     # intro
 
     # form
@@ -185,6 +266,27 @@ elif page=='Diesel Prediction':
     city = st.selectbox("select a Metro city",cities)
     if date and city and st.button("Predict"):
         with open('models/diesel_price_prediction.pk', 'rb') as f:
+            model_dict = pickle.load(f)
+            st.write(model_dict)
+            # convert date to ordinal
+        date_o = date.toordinal()
+        city_dummies = model_dict['city_encoder'].transform([[city]]).toarray()
+        st.write("date as ordinal",date_o)
+        st.write("city as dummy variable", city_dummies)
+        X = np.append(city_dummies[:,:-1],[[date_o]],axis=1)
+        st.write("Our data for input",X)
+        scaled_X = model_dict['scale'].transform(X)
+        result = model_dict['model'].predict(scaled_X)
+        st.write(f"price predicted is {result[0]:.2f}")
+
+elif page=='Petrol Prediction':
+
+    st.header("Predict Petrol Prices")
+    cities = ['Mumbai','Delhi','Chennai','Kolkata']
+    date = st.date_input("select a date")
+    city = st.selectbox("select a Metro city",cities)
+    if date and city and st.button("Predict"):
+        with open('models/petrol_price_prediction.pk', 'rb') as f:
             model_dict = pickle.load(f)
             st.write(model_dict)
             # convert date to ordinal
